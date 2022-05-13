ea9fbe91c3456b3005fe4f236d5a4820908d1289
kutaytire <105226080+kutaytire@users.noreply.github.com>
2022-05-13 21:24:07 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+import model.Activity;
+import model.Person;
+import controller.Controller;
+
+public class App {
+   
+    public static void main(String[] args) {
+        
+        Person m = new Person();
+        Activity a = new Activity();
+        new Controller(m,a);
+       }
+
+}
